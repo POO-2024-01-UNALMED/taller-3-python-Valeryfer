55e5a5076522224772c0ed23e96f3402d9fb66d2
Valeryfer <159395606+Valeryfer@users.noreply.github.com>
2024-02-26 23:53:09 -0500

    Intento 35

--- televisores/control.py
+++ televisores/control.py
@@ -2,10 +2,6 @@ class Control:
     def __init__(self):
         self._tv = None
 
-    def enlazar(self, tv):
-        self._tv = tv
-        tv.setControl(self)
-
     def turnOn(self):
         self._tv.turnOn()
     
@@ -23,15 +19,20 @@ class Control:
 
     def volumenDown(self):
         self._tv.volumenDown()
+
+    def setCanal(self, canal):
+        self._tv.setCanal(canal)
     
-    def getCanal(self, canal):
-        return self._tv.setCanal(canal)
-    
-    def getVolumen(self, volumen):
-        return self._tv.getVolumen(volumen)
+    def setVolumen(self, volumen):
+        self._tv.setVolumen(volumen)
+
+    def enlazar(self, tv):
+        self._tv = tv
+        tv.setControl(self)
     
     def getTv(self):
         return self._tv
     
     def setTv(self, tv):
         self._tv = tv
+    
