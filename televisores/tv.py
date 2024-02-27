b5b44c46c3ec35196a1c993c7936586cabfa41ba
Valeryfer <159395606+Valeryfer@users.noreply.github.com>
2024-02-26 23:25:08 -0500

    Intento 31

--- televisores/tv.py
+++ televisores/tv.py
@@ -1,11 +1,11 @@
 class TV:
     _numTV = 0
     
-    def __init__(self, marca, canal, precio, estado, volumen, control):
+    def __init__(self, marca, estado):
         self._marca = marca
+        self._estado = estado
         self._canal = 1
         self._precio = 500
-        self._estado = estado
         self._volumen = 1
         self._control = None
 
